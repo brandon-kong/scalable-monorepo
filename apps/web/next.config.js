499d7b76d0892f5f0c1a0be20d5389f468839894
Brandon <kongbrandon0@gmail.com>
2025-04-28 18:27:37 -0500

    chore: added build step

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    /* Enables hot reloading for internal packages in the monorepo */
+    transpilePackages: [
+        "@core/ui"
+    ],
+
+    /* We already do linting and typechecking as separate tasks in CI */
     eslint: {
         ignoreDuringBuilds: true,
     },
+    typescript: {
+        ignoreBuildErrors: true,
+    }
 };
 
 export default nextConfig;
