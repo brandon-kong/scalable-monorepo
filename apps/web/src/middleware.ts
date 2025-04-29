7a9dc92a088464c11926718458ad5ed88ca09d86
Brandon <kongbrandon0@gmail.com>
2025-04-29 10:13:50 -0500

    style: formatted

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -2,7 +2,7 @@ import { auth } from "@core/auth";
 
 export default auth((req, res) => {
     console.log(req.auth?.user);
-})
+});
 
 // Read more: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
 export const config = {
