ac5d3c911e1d86388da4936570d0b9c394cccc1c
Brandon <kongbrandon0@gmail.com>
2025-04-28 19:10:43 -0500

    chore: web env validation extends @core/auth env

--- apps/web/src/env.ts
+++ apps/web/src/env.ts
@@ -1,9 +1,10 @@
 import { createEnv } from "@t3-oss/env-nextjs";
 import { vercel } from "@t3-oss/env-nextjs/presets-zod";
+import { env as authEnv } from "@core/auth/env";
 import zod from "zod";
 
 export const env = createEnv({
-    extends: [vercel()],
+    extends: [authEnv, vercel()],
     shared: {
         NODE_ENV: zod
             .enum(["development", "production", "test"])
