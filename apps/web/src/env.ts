877b37e5ebe9ba108d7ef7bc4d13dc16215055fe
Brandon <kongbrandon0@gmail.com>
2025-04-28 19:41:55 -0500

    chore: added dbEnv to env validation

--- apps/web/src/env.ts
+++ apps/web/src/env.ts
@@ -1,10 +1,12 @@
 import { env as authEnv } from "@core/auth/env";
+import { env as dbEnv } from "@core/db/env";
+
 import { createEnv } from "@t3-oss/env-nextjs";
 import { vercel } from "@t3-oss/env-nextjs/presets-zod";
 import zod from "zod";
 
 export const env = createEnv({
-    extends: [authEnv, vercel()],
+    extends: [dbEnv, authEnv, vercel()],
     shared: {
         NODE_ENV: zod
             .enum(["development", "production", "test"])
