4596945c7369ce3ae2b3e08c9581e7df2bb88044
Brandon <kongbrandon0@gmail.com>
2025-04-28 22:44:46 -0500

    chore: added @core/db and adapter

--- packages/auth/src/auth.config.ts
+++ packages/auth/src/auth.config.ts
@@ -1,6 +1,8 @@
 import { type NextAuthConfig } from "next-auth";
 import { baseAuthConfig } from "./base.config";
+import { KyselyAdapter } from "@auth/kysely-adapter";
 
 export const authConfig = {
+    adapter: KyselyAdapter(db)
     ...baseAuthConfig,
 } satisfies NextAuthConfig
