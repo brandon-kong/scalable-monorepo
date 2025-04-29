3e550cbf6a466622eb4911adbfd8b7d038f99639
Brandon <kongbrandon0@gmail.com>
2025-04-28 23:00:34 -0500

    chore: added types

--- apps/web/src/app/api/test/route.ts
+++ apps/web/src/app/api/test/route.ts
@@ -2,5 +2,5 @@ import { db } from "@core/db";
 
 export async function GET(req: Request) {
     const users = await db.selectFrom("User").selectAll().execute();
-    return new Response(JSON.stringify(users))
+    return new Response(JSON.stringify(users));
 }
--- packages/auth/src/auth.config.ts
+++ packages/auth/src/auth.config.ts
@@ -1,8 +1,9 @@
 import { type NextAuthConfig } from "next-auth";
 import { baseAuthConfig } from "./base.config";
 import { KyselyAdapter } from "@auth/kysely-adapter";
+import { db } from "@core/db";
 
 export const authConfig = {
-    adapter: KyselyAdapter(db)
+    adapter: KyselyAdapter(db as any),
     ...baseAuthConfig,
 } satisfies NextAuthConfig
