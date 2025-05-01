217f571c6dc99efcde53930af9f83c178008184a
Brandon <kongbrandon0@gmail.com>
2025-05-01 11:46:10 -0500

    chore: verify page redirects to login page if not valid

--- apps/web/src/app/auth/(unprotected)/verify/page.tsx
+++ apps/web/src/app/auth/(unprotected)/verify/page.tsx
@@ -2,13 +2,21 @@
 
 import { Button } from "@core/ui/next/button";
 import Typography from "@core/ui/typography";
+import { redirect, useSearchParams } from "next/navigation";
 
 type FormValues = {
     email: string;
 };
 
 export default function LoginPage() {
-    // set up the form schema
+    const searchParams = useSearchParams();
+
+    const authType = searchParams.get("type");
+
+    if (authType !== "email") {
+        return redirect("/auth/login");
+    }
+
     return (
         <main className={"p-20 max-w-2xl mx-auto flex flex-col gap-4"}>
             <Typography variant={"h2"}>We sent you an email</Typography>
